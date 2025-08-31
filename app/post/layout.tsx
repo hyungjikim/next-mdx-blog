97187c697568a40e0467fb6a07ffa3fae6ea3caa
hyungji.k <icthelight@naver.com>
2025-08-31 19:32:36 +0900

    feat: 포스트 레이아웃에 홈 앵커 추가

--- app/post/layout.tsx
+++ app/post/layout.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import UtterancesComments from "../_shared/components/UtterancesComments";
+import { Home } from "lucide-react";
 
 export default function PostLayout({
   children,
@@ -7,6 +9,12 @@ export default function PostLayout({
 }) {
   return (
     <>
+      <Link
+        href="/"
+        className="flex items-center gap-2 hover:text-gray-500 transition-colors"
+      >
+        <Home /> <span>홈으로</span>
+      </Link>
       {children}
       <UtterancesComments key="post-comments" repo="hyungjikim/next-mdx-blog" />
     </>
